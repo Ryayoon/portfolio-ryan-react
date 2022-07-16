644926ef5a2a52a193d7a904cfe438309ea49476
Ryan Yoon <ryayoon7gzf@gmail.com>
2022-07-16 17:37:03 +1200

    added awards section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,10 +3,9 @@ import emoji from '../../assets/images/Emoji.png';
 const Home = () => {
 
     return (
-        <>
-            <img className='self-emoji' src={emoji} alt="my-emoji"/>
         <div className="home">
-            <div className="text">
+            <img className='self-emoji' src={emoji} alt="my-emoji"/>
+            <div className="text-one">
                 <h2>Hey there, I'm Ryan.</h2>
                 <p>I am currently a Part II Computer Systems Engineering Student @ 
                     The University of Auckland looking for potential Internship 
@@ -14,8 +13,20 @@ const Home = () => {
                     manner.
                 </p>
             </div>
+            <div className="text-two">
+                <h2>Certification & Awards</h2>
+                <br/>
+                <hr className="indicate"/>
+                <br/>
+                <h3 className="cert-awards">AWS Cloud Practitioner - Certificate</h3>
+                <br/>
+                <hr className="indicate"/>
+                <br/>
+                <h3 className="cert-awards">Best Technical Implementation @ Devs Hackathon - Award</h3>
+                <br/>
+                <hr className="indicate"/>
+            </div>
         </div>
-        </>
      );
 }
  
