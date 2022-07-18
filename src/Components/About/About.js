335fd2fab124318ce55b06c2fd4ef5712d8abe50
Ryan Yoon <ryayoon7gzf@gmail.com>
2022-07-19 01:18:19 +1200

    updated-with-help

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,36 +5,49 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 const About = () => {
     return (
         <div className="about">
-            <div className="cube-spinner">
-                <div className="face1">
-                    <FontAwesomeIcon icon={faPython} color="#050e0b" />
+            <div className="flex justify-between content-evenly items-center md:items-stretch flex-col-reverse md:flex-row">
+
+                <div className="text-2">
+                    <h2 className="self-title">About Myself</h2>
+                    <br/>
+                    <p className="paragraph-1 max-w-[300px]">Currently, as a Certified AWS Cloud Practitioner I am studying
+                        towards the Solutions Architect role. In terms of programming experiences I had implemented
+                        some
+                        basic frontend web development (using HTML, CSS, a bit of React and javascript)
+                        in the past. Through high-school projects and University courses I had also learned how to
+                        code
+                        some Python, C, Java(Object Oriented Programming) and MATLAB.</p>
                 </div>
-                <div className="face2">
-                    <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                <div className="flex flex-col justify-between -mb-[150px] md:mb-0 -mr-[150px] md:mr-0 -z-[1]">
+
+                    <div className="w-[150px] h-[150px]">
+                        <div className="cube-spinner">
+                            <div className="face1">
+                                <FontAwesomeIcon icon={faPython} color="#050e0b"/>
+                            </div>
+                            <div className="face2">
+                                <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                            </div>
+                            <div className="face3">
+                                <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                            </div>
+                            <div className="face4">
+                                <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                            </div>
+                            <div className="face5">
+                                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                            </div>
+                            <div className="face6">
+                                <FontAwesomeIcon icon={faJava} color="#f70d0c"/>
+                            </div>
+                        </div>
+                    </div>
+                    <FontAwesomeIcon className="aws-logo" icon={faAws} color="#ec912d"/>
+
                 </div>
-                <div className="face3">
-                    <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                </div>
-                <div className="face4">
-                    <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                </div>
-                <div className="face5">
-                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                </div>
-                <div className="face6">
-                    <FontAwesomeIcon icon={faJava} color="#f70d0c" />
-                </div>
+
             </div>
-            <div className="text-2">
-                <h2 className="self-title">About Myself</h2>
-                <br/>
-                <p className="paragraph-1">Currently, as a Certified AWS Cloud Practitioner I am studying
-                    towards the Solutions Architect role. In terms of programming experiences I had implemented some
-                    basic frontend web development (using HTML, CSS, a bit of React and javascript)
-                    in the past. Through high-school projects and University courses I had also learned how to code
-                    some Python, C, Java(Object Oriented Programming) and MATLAB.</p>
-            </div>
-            <FontAwesomeIcon className="aws-logo" icon={faAws} color="#ec912d" />
+
             <div className="text-3">
                 <h2 className="job-title">Work Experience</h2>
                 <br/>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,59 +4,59 @@ import emoji from '../../assets/images/Emoji.png';
 const Home = () => {
 
     return (
-        <>
-        <div className="home">
-            <img className='text-zero self-emoji' src={emoji} alt="my-emoji"/>
-            <div className="text-one">
-                <h2>Hey there, I'm Sukju.</h2>
-                <p>I am currently a Part II Computer Systems Engineering Student @ 
-                    The University of Auckland looking for potential Internship 
-                    opportunities to gain practical experiences in the industry in a professional 
-                    manner.
-                </p>
+            <div className="home">
+                <div className="flex justify-between content-center items-center flex-col-reverse md:flex-row">
+                    <div className="text-one max-w-[300px]">
+                        <h2>Hey there, I'm Sukju.</h2>
+                        <p>I am currently a Part II Computer Systems Engineering Student @
+                            The University of Auckland looking for potential Internship
+                            opportunities to gain practical experiences in the industry in a professional
+                            manner.
+                        </p>
+                    </div>
+                    <img className='text-zero w-[250px] md:w-[350px] -mb-[50px] md:mb-0 -mt-[50px] -mr-[100px]' src={emoji} alt="my-emoji"/>
+
+                </div>
+                <div className="text-two">
+                    <h2>Certifications & Awards</h2>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">AWS Cloud Practitioner - Certificate</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">NCEA Level 1 & 2 & 3 Excellence - Certificate</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">Best Technical Implementation @ Devs Hackathon - Award</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">Certificate of Outstanding Achievement (COMPSCI 101) - Award</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">Prize for NCEA Level 1 Mathematics - Award</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                </div>
+                <div className="text-three">
+                    <h2>Projects</h2>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">Hack for Happiness Project (News App)</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                    <br/>
+                    <h4 className="cert-awards">Visual Cryptography (MATLAB)</h4>
+                    <br/>
+                    <hr className="indicate"/>
+                </div>
             </div>
-            <div className="text-two">
-                <h2>Certifications & Awards</h2>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">AWS Cloud Practitioner - Certificate</h4>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">NCEA Level 1 & 2 & 3 Excellence - Certificate</h4>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">Best Technical Implementation @ Devs Hackathon - Award</h4>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">Certificate of Outstanding Achievement (COMPSCI 101) - Award</h4>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">Prize for NCEA Level 1 Mathematics - Award</h4>
-                <br/>
-                <hr className="indicate"/>
-            </div>
-            <div className="text-three">
-                <h2>Projects</h2>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">Hack for Happiness Project (News App)</h4>
-                <br/>
-                <hr className="indicate"/>
-                <br/>
-                <h4 className="cert-awards">Visual Cryptography (MATLAB)</h4>
-                <br/>
-                <hr className="indicate"/>
-            </div>
-        </div>
-    <Loader type="ball-spin-fade-loader" />
-    </>
-     );
+    );
 }
- 
+
 export default Home;
